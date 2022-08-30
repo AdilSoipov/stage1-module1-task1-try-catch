3d573beab03f68d8386e106c2648be8db89259d8
AdilSoipov <111345038+AdilSoipov@users.noreply.github.com>
2022-08-30 14:37:05 +0500

    Update ParseIntegers.java

--- src/main/java/com/epam/m1/exceptions/ParseIntegers.java
+++ src/main/java/com/epam/m1/exceptions/ParseIntegers.java
@@ -21,8 +21,14 @@ public class ParseIntegers {
         String justWords = "";
         while (words.hasNext()) {
             String next = words.next();
+            try{
             int number = Integer.parseInt(next);
-            // todo: complete it
+            sum += number;
+            }
+            catch(Exception e){
+                justWords += " " + next;
+                
+            }
         }
         System.out.println("Sum is " + sum);
         System.out.println("Just words:" + justWords);
